4b31b20194d49b0b909464014e03df7b38347286
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-15 23:40:47 -0300

    Exporting use-local-storage-context

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,5 @@
 export { default as useChangeKeyWhenFalsy } from './useChangeKeyWhenFalsy';
 export { default as useFormField } from './useFormField';
 export { default as useIsExternalUrl } from './useIsExternalUrl';
+export { default as useLocalStorage } from './useLocalStorage';
 export { default as useToast } from './useToast';
