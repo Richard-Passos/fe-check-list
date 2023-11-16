5e1f7c32f0570b0f85f67cdb7fc709357cf44436
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-16 18:23:49 -0300

    Removing unnecessary code

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,3 @@
-export { default as Carousel } from './carousel';
 export { default as CheckList } from './check-list';
-export { default as ComponentsNav } from './components-nav';
 export { default as Header } from './header';
 export { default as InputMask } from './input-mask';
-export { default as Logo } from './logo';
