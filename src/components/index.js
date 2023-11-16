b2e66a163e84cfa7dd828628474a38835c2313ad
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-16 15:39:23 -0300

    Exporting input-mask

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { default as Carousel } from './carousel';
 export { default as CheckList } from './check-list';
 export { default as ComponentsNav } from './components-nav';
 export { default as Header } from './header';
+export { default as InputMask } from './input-mask';
 export { default as Logo } from './logo';
