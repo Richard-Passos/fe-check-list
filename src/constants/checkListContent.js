b5b17f19d942440ea7a14a58504ff456b0c076fa
Richard P <richard.ac.passos12@gmail.com>
2023-12-04 15:53:26 -0300

    Update checkListContent.js

--- src/constants/checkListContent.js
+++ src/constants/checkListContent.js
@@ -7,112 +7,112 @@ const checkListContent = [
         qnt: 2,
         item: 'talabarte',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'talabarte com amortecedor',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'cinto paraquedista',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'corda',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'Batedor',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'Par colocador de cunha',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'Gabarito',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: '-',
         item: 'Ferrinho de limpeza',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'removedor de forma',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Régua',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Ponteiro pequeno',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Talhadeira grande',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Talhadeira pequena',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Alicate de bico angular',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Alicate reto',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Porta cunha e cunha tripartida',
         go: true,
-        back: false,
+        back: true,
       },
     ],
   },
@@ -125,105 +125,105 @@ const checkListContent = [
         qnt: 1,
         item: 'Trena 50 m',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: '-',
         item: 'Giz',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Policorte',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'Alicate cortador de capa',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'fita crepe',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Bloco de pré-blocagem',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 3,
         item: 'Pote de graxa',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'Espatula pequena',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'Torques',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Saco de prego 18x30',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'dico res. Policorte',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'ferro de pré-blocagem',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'gabarito de pré-blocagem',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'macaco de pré-blocagem',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'kit de pré-blocagem reserva',
         go: true,
-        back: false,
+        back: true,
       },
     ],
   },
@@ -236,321 +236,321 @@ const checkListContent = [
         qnt: 1,
         item: 'Adaptador t',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Canivete',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'escova',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'fita isolante',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'garrafa de thiner',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'lanterna',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'lima chata',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'lima redonda',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'pincel',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'serrinha',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'spray',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'trena 3m',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'veda rosca',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: '-',
         item: 'vela de cera',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'alicate universal',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'chave de boca 18/19',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'chave de fenda',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'chave inglesa',
         go: true,
-        back: false,
+        back: true,
       },
       {
         qnt: 2,
         item: 'chave philips',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'escova de aço',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'estilete',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'martelo',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Marreta',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'refil de estilete',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'torques',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 5,
         item: 'adaptador canhão',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: '-',
         item: 'arame',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'chave esmerilhadeira',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 20,
         item: 'cunhas de meia com anel',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 20,
         item: 'cunhas de meia sem anel',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 20,
         item: 'cunhas de 5/8 com anel',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 20,
         item: 'cunhas de 5/8 sem anel',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'capas de chuva',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'disco reserva da esmerilhadeira ',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: '-',
         item: 'estopa',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'extensão trifásica',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'funil',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'galão de óleo',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'kit removedor de cunha',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 4,
         item: 'chave allen',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Talhadeira pequena',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'Trena 5m',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'esmerilhadeira',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'extensão bifásica',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'lona',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'transformador',
         go: true,
-        back: false,
+        back: true,
       },
     ],
   },
@@ -563,35 +563,35 @@ const checkListContent = [
         qnt: 1,
         item: 'alavanca',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'bomba Deltapac',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'carrinho',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'macaco reserva',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'macaco Deltapac',
         go: true,
-        back: false,
+        back: true,
       },
     ],
   },
@@ -604,42 +604,42 @@ const checkListContent = [
         qnt: 1,
         item: 'acionador',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'alicate jacaré',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'bomba Prepon',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'chave allen de sangrar',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 1,
         item: 'macaco Prepon',
         go: true,
-        back: false,
+        back: true,
       },
 
       {
         qnt: 2,
         item: 'manômetro',
         go: true,
-        back: false,
+        back: true,
       },
     ],
   },
